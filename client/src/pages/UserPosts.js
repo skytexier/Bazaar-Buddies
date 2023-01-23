2b36119239748455f0b63748fa30804ce4427ba3
Jongwon Lee <jongwonlee12345@gmail.com>
2023-01-22 18:46:11 -0800

    fixes

--- client/src/pages/UserPosts.js
+++ client/src/pages/UserPosts.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import {Container, Row, Col, Form, Button} from 'react-bootstrap';
-import { ProductCard } from '../components/ProductCard.js';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../utils/queries';
+// import React, { useEffect } from 'react';
+// import { useState } from 'react';
+// import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+// import { ProductCard } from '../components/ProductCard.js';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_PRODUCTS } from '../utils/queries';
 
-function UserPosts(){
-    const {loading, data} = useQuery(QUERY_PRODUCTS);
-    console.log(data)
+// function UserPosts(){
+//     const {loading, data} = useQuery(QUERY_PRODUCTS);
+//     console.log(data)
 
-    return (
-        <Col className='product_box' lg={12}>
-          <section className="products" id="products">
-            <h2>User Posted Items</h2>
-            <Container className='subcategories'>
-            <h4>Major Categories</h4>
-            </Container>
-        <Row className="product_grid">
-          {data.map((item, index) => {
-            return <ProductCard
-            key={index}
-            {...item}
-            />
-          })}
-          </Row>
-          </section>
-        </Col>
-      );
-}
+//     return (
+//         <Col className='product_box' lg={12}>
+//           <section className="products" id="products">
+//             <h2>User Posted Items</h2>
+//             <Container className='subcategories'>
+//             <h4>Major Categories</h4>
+//             </Container>
+//         <Row className="product_grid">
+//           {data.map((item, index) => {
+//             return <ProductCard
+//             key={index}
+//             {...item}
+//             />
+//           })}
+//           </Row>
+//           </section>
+//         </Col>
+//       );
+// }
 
-export default UserPosts;
+// export default UserPosts;
